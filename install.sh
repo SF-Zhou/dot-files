ffce7ba6039383b9151946f65697c44ba50901d4
SF-Zhou <sfzhou.scut@gmail.com>
2016-12-12 13:16:07 +0800

    updated install.sh for git & zsh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
+# Git
+echo Set Git
+rm -f ~/.gitconfig
+ln -s `pwd`/Git/git.config ~/.gitconfig
+
 # Hammerspoon
 echo Set Hammerspoon 
 mkdir -p ~/.hammerspoon
 rm -f ~/.hammerspoon/init.lua
 ln -s `pwd`/Hammerspoon/init.lua ~/.hammerspoon/init.lua
+
+# ZSH + oh-my-zsh
+echo Set ZSH
+rm -f ~/.zshrc
+ln -s `pwd`/ZSH/zshrc ~/.zshrc
