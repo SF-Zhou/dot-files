c9e100a6ee4553f126465189f13c51468feb0583
SF-Zhou <sfzhou.scut@gmail.com>
2016-12-13 20:33:28 +0800

    added window layout set in HammerSpoon script

--- Hammerspoon/init.lua
+++ Hammerspoon/init.lua
@@ -1,40 +1,85 @@
-ctrl_table = {
-    sends_escape = true,
-    last_mods = {}
-}
-
-control_key_timer = hs.timer.delayed.new(0.15, function()
-    ctrl_table["send_escape"] = false
-    -- log.i("timer fired")
-    -- control_key_timer:stop()
+function table.length( tbl )
+  local count = 0
+  for _ in pairs(tbl) do count = count + 1 end
+  return count
 end
-)
 
-last_mods = {}
-
+saved = {ready=false, last_is_empty=true}
 control_handler = function(evt)
-  local new_mods = evt:getFlags()
-  if last_mods["ctrl"] == new_mods["ctrl"] then
-      return false
-  end
-  if not last_mods["ctrl"] then
-    -- log.i("control pressed")
-    last_mods = new_mods
-    ctrl_table["send_escape"] = true
-    -- log.i("starting timer")
-    control_key_timer:start()
-  else
-    -- log.i("contrtol released")
-    -- log.i(ctrl_table["send_escape"])
-    if ctrl_table["send_escape"] then
-      -- log.i("send escape key...")
-      hs.eventtap.keyStroke({}, "ESCAPE")
+    local current = evt:getFlags()
+
+    if saved["ready"] and not next(current) then
+        hs.alert.show('ESC')
+        saved["ready"] = false
+        saved["last_is_empty"] = true
+        return false
+    else
+        saved["ready"] = false
     end
-    last_mods = new_mods
-    control_key_timer:stop()
-  end
-  return false
+
+    if current["ctrl"] and saved["last_is_empty"] and table.length(current) == 1 then
+        saved["ready"] = true
+    end
+
+    if not next(current) then
+        saved["last_is_empty"] = true
+    else
+        saved["last_is_empty"] = false
+    end
+
+    return false
 end
 
-control_tap = hs.eventtap.new({12}, control_handler)
-control_tap:start()
+key_handler = function(evt)
+    saved["ready"] = false
+    return false
+end
+
+hs.eventtap.new({10, 11}, key_handler):start()
+hs.eventtap.new({12}, control_handler):start()
+
+-- disalbe animations
+hs.window.animationDuration = 0
+
+-- for whole window
+hs.hotkey.bind({"cmd", "alt"}, "Up", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  win:setFrame(max)
+end)
+
+-- for left half window
+hs.hotkey.bind({"cmd", "alt"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- for right half window
+hs.hotkey.bind({"cmd", "alt"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- reload config
+hs.hotkey.bind({"cmd", "alt"}, "Down", function()
+  hs.reload()
+end)
+hs.alert.show("Config loaded")
