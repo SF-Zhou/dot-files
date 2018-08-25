e74a47424b160d38d6251e13b563fbf7121e1d35
SF-Zhou <sfzhou.scut@gmail.com>
2018-08-25 18:53:28 +0800

    Add apt software list

---
+++ apt/setup.sh
@@ -0,0 +1,4 @@
+sudo apt -y install git vim-gtk xcape proxychains xclip tsocks
+sudo apt -y install libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
+sudo apt -y install supervisor polipo
+sudo apt -y install python3-opencv
