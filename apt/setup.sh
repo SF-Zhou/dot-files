cd07c77664782a3fa138d74a62c1f815bc9fabcd
SF-Zhou <sfzhou.scut@gmail.com>
2018-08-25 19:39:55 +0800

    Update shadow config & setup

--- apt/setup.sh
+++ apt/setup.sh
@@ -2,3 +2,8 @@ sudo apt -y install git vim-gtk xcape proxychains xclip tsocks
 sudo apt -y install libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
 sudo apt -y install supervisor polipo
 sudo apt -y install python3-opencv
+
+sudo mkdir -p /etc/supervisor/conf.d
+sudo cp ./supervisor/*.conf /etc/supervisor/conf.d
+sudo cp ./etc_config/proxychains.conf /etc/
+sudo unzip ./etc_config/shadow_config.zip -e /etc/
