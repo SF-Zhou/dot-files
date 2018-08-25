0db2d7f547308459bfc2948f28537e2eb3cc667c
SF-Zhou <sfzhou.scut@gmail.com>
2018-08-25 16:03:15 +0800

    Add zsh highlight config

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -6,6 +6,7 @@ CURRENT="$( cd "$(dirname "$0")" ; pwd -P )"
 echo $TASK start
 if ! test -d $HOME/.oh-my-zsh; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 fi
 rm -f $HOME/.zshrc && ln -s $CURRENT/.zshrc $HOME/.zshrc
 echo $TASK finish
