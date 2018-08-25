1f92f402a7384d22be886b87b9922bcafaa43bb2
SF-Zhou <sfzhou.scut@gmail.com>
2018-08-25 17:08:44 +0800

    Add pure theme & highlight in zsh setup

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,10 +3,22 @@
 TASK="setup zsh"
 CURRENT="$( cd "$(dirname "$0")" ; pwd -P )"
 
+OH_MY_ZSH=$HOME/.oh-my-zsh
+FUNCTIONS=$OH_MY_ZSH/functions
+PLUGINS=$OH_MY_ZSH/custom/plugins
+HIGHLIGHT=$PLUGINS/zsh-syntax-highlighting
+PURE=$PLUGINS/pure
+
 echo $TASK start
-if ! test -d $HOME/.oh-my-zsh; then
+if ! test -d $OH_MY_ZSH; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HIGHLIGHT --depth 1
+  git clone https://github.com/sindresorhus/pure.git $PURE --depth 1
 fi
+
+mkdir -p $FUNCTIONS
+rm -f $FUNCTIONS/pure  && ln -s $PURE/pure.zsh  $FUNCTIONS/pure
+rm -f $FUNCTIONS/async && ln -s $PURE/async.zsh $FUNCTIONS/async
+
 rm -f $HOME/.zshrc && ln -s $CURRENT/.zshrc $HOME/.zshrc
 echo $TASK finish
