b165c21481140a2429780b08ecb09da710421046
SF-Zhou <sfzhou.scut@gmail.com>
2018-09-12 11:32:05 +0800

    Add gcc & g++ version switch script

---
+++ g++/setup.sh
@@ -0,0 +1,8 @@
+VERSION=$1
+VERSION=${VERSION:=4.9}
+GCC_VERSION=gcc-$VERSION
+GPP_VERSION=g++-$VERSION
+
+sudo rm -rf /usr/bin/gcc /usr/bin/g++
+sudo ln -s /usr/bin/$GCC_VERSION /usr/bin/gcc
+sudo ln -s /usr/bin/$GPP_VERSION /usr/bin/g++
