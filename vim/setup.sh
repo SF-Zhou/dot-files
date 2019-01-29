688e858a1553434678a712a07623542efe4e7d7d
SF-Zhou <sfzhou.scut@gmail.com>
2019-01-29 13:59:30 +0800

    Upgrade vim plugin manager to vim-plug

--- vim/setup.sh
+++ vim/setup.sh
@@ -4,8 +4,9 @@ TASK="setup vim"
 CURRENT="$( cd "$(dirname "$0")" ; pwd -P )"
 
 echo $TASK start
-if ! test -d $HOME/.vim/bundle/Vundle.vim; then
-    git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+if ! test -f $HOME/.vim/autoload/plug.vim; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
 rm -f $HOME/.vimrc && ln -s $CURRENT/.vimrc $HOME/.vimrc
 echo $TASK finish
