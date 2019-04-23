5eccf467ba8dc1ac2eca41bed95b5b33353826bd
SF-Zhou <sfzhou.scut@gmail.com>
2019-04-23 16:01:53 +0800

    Fix tmux setup

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -4,5 +4,5 @@ TASK="setup tmux"
 CURRENT="$( cd "$(dirname "$0")" && pwd -P )"
 
 echo "$TASK" start
-rm -f "$HOME"/.zshrc && ln -s "$CURRENT"/.tmux.conf "$HOME"/.tmux.conf
+rm -f "$HOME"/.tmux.conf && ln -s "$CURRENT"/.tmux.conf "$HOME"/.tmux.conf
 echo "$TASK" finish
