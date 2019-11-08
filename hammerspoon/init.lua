0ce306a8b402829c90082417a0eca1a739068df0
SF-Zhou <sfzhou.scut@gmail.com>
2019-11-08 11:07:39 +0800

    Add mouse simulation in hammerspoon just because my wireless mouse is dead😂

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -47,4 +47,36 @@ end)
 hs.hotkey.bind({"cmd", "shift"}, "Down", function()
   hs.reload()
 end)
+
+-- simulate mouse
+function mouse_down()
+  pos = hs.mouse.getAbsolutePosition()
+  pos.y = pos.y + 20
+  hs.mouse.setAbsolutePosition(pos)
+end
+function mouse_up()
+  pos = hs.mouse.getAbsolutePosition()
+  pos.y = pos.y - 20
+  hs.mouse.setAbsolutePosition(pos)
+end
+function mouse_left()
+  pos = hs.mouse.getAbsolutePosition()
+  pos.x = pos.x - 20
+  hs.mouse.setAbsolutePosition(pos)
+end
+function mouse_right()
+  pos = hs.mouse.getAbsolutePosition()
+  pos.x = pos.x + 20
+  hs.mouse.setAbsolutePosition(pos)
+end
+
+hs.hotkey.bind({"option"}, "J", mouse_down, nil, mouse_down)
+hs.hotkey.bind({"option"}, "K", mouse_up, nil, mouse_up)
+hs.hotkey.bind({"option"}, "H", mouse_left, nil, mouse_left)
+hs.hotkey.bind({"option"}, "L", mouse_right, nil, mouse_right)
+hs.hotkey.bind({"option"}, "return", function()
+  pos = hs.mouse.getAbsolutePosition()
+  hs.eventtap.leftClick(pos)
+end)
+
 hs.alert.show("Config loaded")
